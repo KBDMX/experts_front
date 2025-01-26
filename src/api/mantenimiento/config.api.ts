0f159cfcb42643ed1d4d9fba3ca9daf94302bdac
kennyp2233 <kennyp41234@gmail.com>
2025-01-26 18:33:12 -0500

    fix: dotenv

--- src/api/mantenimiento/config.api.ts
+++ src/api/mantenimiento/config.api.ts
@@ -1,1 +1,5 @@
+import dotenv from 'dotenv';
+
+// Cargar variables de entorno desde el archivo .env
+dotenv.config();
 export const baseUrl = process.env.BASE_URL || 'http://localhost:3001/api/v1';
