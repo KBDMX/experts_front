f7c7dbf95cb609451ffd87446fc352ccb572c0c8
Kenny P <kennyp2233@gmail.com>
2025-01-29 15:19:04 -0500

    fix: base url env

--- src/api/mantenimiento/config.api.ts
+++ src/api/mantenimiento/config.api.ts
@@ -1,4 +1,1 @@
-export const baseUrl =
-    process.env.VERCEL_ENV === 'production'
-        ? 'https://experts-back.onrender.com/api/v1' // URL de producción en Vercel
-        : 'http://localhost:3001/api/v1'; // URL local en desarrollo
+export const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001/api/v1';
